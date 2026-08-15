e5935395a0923c84a2c045c8377597195d57461f
huynhbaduydn-arch <huynhbaduydn@gmail.com>
2026-08-15 07:59:17 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,8 +2,8 @@
 
 // Tệp sinh cục bộ; không chứa Gmail rõ, token hay client secret.
 window.TPT_APP_CONFIG = Object.freeze({
-  "APP_VERSION": "3.2.0",
-  "BUILD_ID": "20260815-smart-import-v10",
+  "APP_VERSION": "3.1.0-rc.1",
+  "BUILD_ID": "20260814-drive-rc1",
   "SCHEMA_VERSION": 10,
   "APP_ID": "vn.giaoducso40.tpt.thcs.thcs",
   "SCHOOL_PROFILE_ID": "thcs-thcs-profile-001",
@@ -14,6 +14,6 @@ window.TPT_APP_CONFIG = Object.freeze({
   "GOOGLE_CLIENT_ID": "422851861968-emastosfnml2m0d8pp38m4hoovh2ihu3.apps.googleusercontent.com",
   "EXPECTED_GOOGLE_EMAIL_SHA256": "3bd4a97daab011dc261cfdb42ede08779f2298d849821a758c0aa9ee1c3c413b",
   "EXPECTED_DRIVE_PERMISSION_ID": "",
-  "REPOSITORY_NAME": "TROLYTONGPHUTRACH",
+  "REPOSITORY_NAME": "thcsTROLYTONGPHUTRACH",
   "BASE_PATH": "./"
 });
